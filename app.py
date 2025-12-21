43117ef095a28928f251cde07e80eaee2835d8dc
Dheeraj <dheerajsaroha2892002@gmail.com>
2025-12-22 02:14:41 +0530

    Added Flask API for resume-job matching

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask,request, jsonify
+import os
+
+from utils.preprocess import clean_text, extract_text_from_pdf
+from model.matcher import calculate_match
+
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = "data"
+os.makedirs(UPLOAD_FOLDER,exist_ok=True)
 
 @app.route("/")
 def home():
     return "AI Resume Job Matcher is running!"
+
+@app.route("/match",methods=["POST"])
+def match_job_resume():
+    if "resume" not in request.files or "job_description" not in request.form:
+        return jsonify({"error":"Resume file and job description are required"}),400
+    resume_file = request.files["resume"]
+    job_description = request.form["job_description"]
+
+    resume_path = os.path.join(UPLOAD_FOLDER,resume_file.filename)
+    resume_file.save(resume_path) # Save the file
+
+    resume_text = extract_text_from_pdf(resume_path)
+    resume_clean = clean_text(resume_text)
+    job_clean = clean_text(job_description)
+
+    score, keywords = calculate_match(resume_clean,job_clean)
+    return jsonify({
+        "match_score":score,
+        "matching_keywords":keywords
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
